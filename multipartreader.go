91ebf9cab1d1bbcc92a7a3e546d10d35b5fb7239
pikoUsername <70123212+pikoUsername@users.noreply.github.com>
2021-08-20 16:00:19 +0600

    Update multipartreader.go
    update

--- multipartreader.go
+++ multipartreader.go
@@ -1,12 +1,7 @@
 // Package multipartreader helps you encode large files in MIME multipart format
 // without reading the entire content into memory.
 
-/*
-魔改multipartreader
-https://github.com/iikira/BaiduPCS-Go/blob/master/requester/multipartreader/multipartreader.go
-iikira
-Apache License 2.0
-*/
+// p.s - multipartreader + multipartstreamer
 
 package multipartreader
 
@@ -14,6 +9,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"os"
@@ -21,19 +17,20 @@ import (
 	"sync/atomic"
 )
 
-//MultipartReader implements io.Reader, can be used to encode large files
+// MultipartReader implements io.Reader, can be used to encode large files
 type MultipartReader struct {
 	contentType string
-	Boundary    string
+	boundary    string
 
+	writer      *multipart.Writer
 	readers     []io.Reader
 	length      int64
 	multiReader io.Reader
 	count       int64
 }
 
-//NewMultipartReader creates new MultipartReader
-func NewMultipartReader() (mr *MultipartReader) {
+// New creates new MultipartReader
+func New() (mr *MultipartReader) {
 	buf := bytes.NewBuffer(nil)
 	writer := multipart.NewWriter(buf)
 
@@ -44,62 +41,96 @@ func NewMultipartReader() (mr *MultipartReader) {
 	closeReader := strings.NewReader(formClose)
 
 	mr = &MultipartReader{
+		writer:      writer,
 		contentType: writer.FormDataContentType(),
-		Boundary:    writer.Boundary(),
+		boundary:    writer.Boundary(),
 		readers:     []io.Reader{bodyReader, closeReader},
 		length:      int64(len(formBody) + len(formClose)),
 	}
 	return
 }
 
-//AddReader adds new reader to MultipartReader
+// SetBoundary method is multipart.Writer.SetBoundary copy
+func (w *MultipartReader) SetBoundary(boundary string) (err error) {
+	err = w.writer.SetBoundary(boundary)
+	return
+}
+
+// AddReader adds new reader to MultipartReader
 func (mr *MultipartReader) AddReader(r io.Reader, length int64) {
 	i := len(mr.readers)
 	mr.readers = append(mr.readers[:i-1], r, mr.readers[i-1])
 	mr.length = mr.length + length
 }
 
-//AddFormReader adds new reader as form part to MultipartReader
+// AddFormReader adds new reader as form part to MultipartReader
 func (mr *MultipartReader) AddFormReader(r io.Reader, name, filename string, length int64) {
-	form := fmt.Sprintf("--%s\r\nContent-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n\r\n", mr.Boundary, name, filename)
+	form := fmt.Sprintf("--%s\r\nContent-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n\r\n", mr.boundary, name, filename)
 	mr.AddReader(strings.NewReader(form), int64(len(form)))
 	mr.AddReader(r, length)
 	return
 }
 
-//AddFile adds new file to MultipartReader
+// WriteFields writes multiple form fields to the multipart.Writer.
+func (m *MultipartReader) AddFields(fields map[string]string) error {
+	var err error
+
+	for key, value := range fields {
+		err = m.writer.WriteField(key, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// AddFile adds new file to MultipartReader
 func (mr *MultipartReader) AddFile(file *os.File) (err error) {
 	fs, err := file.Stat()
 	if err != nil {
 		return
 	}
 
-	form := fmt.Sprintf("--%s\r\nContent-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n\r\n", mr.Boundary, "file", fs.Name())
+	form := fmt.Sprintf("--%s\r\nContent-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n\r\n", mr.boundary, "file", fs.Name())
 	mr.AddReader(strings.NewReader(form), int64(len(form)))
 	mr.AddReader(file, fs.Size())
 	return
 }
 
-//SetupHTTPRequest set multiReader and headers after adding readers
-func (mr *MultipartReader) SetupHTTPRequest(req *http.Request) {
-	mr.multiReader = io.MultiReader(mr.readers...)
-
+// SetupHTTPRequest set multiReader and headers after adding readers
+func (mr *MultipartReader) SetupRequest(req *http.Request) {
+	req.Body = mr.GetCloseReader()
 	req.Header.Add("Content-Type", mr.contentType)
 	req.ContentLength = mr.length
 }
 
-//Read implements the Read method
-func (mr *MultipartReader) Read(p []byte) (n int, err error) {
-	n, err = mr.multiReader.Read(p)
-	atomic.AddInt64(&mr.count, int64(n))
+// Read implements the Read method
+func (mpr *MultipartReader) Read(p []byte) (n int, err error) {
+	mr := mpr.GetMultiReader()
+	n, err = mr.Read(p)
+	atomic.AddInt64(&mpr.count, int64(n))
 	return n, err
 }
 
-//Count returns length of read data
+// Count returns length of read data
 func (mr *MultipartReader) Count() int64 {
 	return atomic.LoadInt64(&mr.count)
 }
 
+// ContentType returns contentType
 func (mr *MultipartReader) ContentType() string {
 	return mr.contentType
+}
+
+func (mr *MultipartReader) GetMultiReader() io.Reader {
+	if mr.multiReader == nil {
+		mr.multiReader = io.MultiReader(mr.readers...)
+	}
+	return mr.multiReader
+}
+
+func (mr *MultipartReader) GetCloseReader() io.ReadCloser {
+	reader := mr.GetMultiReader()
+	return ioutil.NopCloser(reader)
 }
